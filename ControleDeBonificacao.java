4876b7ac773d71d6a91faa32b02f490bac7e0dd2
Raphael Leal <raphaelleal990@gmail.com>
2021-09-24 01:55:15 +0000

    Atualização

--- ControleDeBonificacao.java
+++ ControleDeBonificacao.java
@@ -1,10 +1,12 @@
 public class ControleDeBonificacao {
+    //Declaração de classe ControleDeBonificacao;
 	private double totalDebonus;
-	
+    //Encapsulamento da variável totalDebonus ;
+	//Consultar total de bônus pelo metódo get;
 	public double getTotalDeBonus() {
 		return this.totalDebonus;
 	}
-	
+	  //Calculando o valor atual de total de bônus + bonifica;
 	public double calculaTotalDeBonus(Funcionario f) {
 		return this.totalDebonus = this.totalDebonus + f.bonifica();
 	}
--- Gerente.java
+++ Gerente.java
@@ -1,25 +1,31 @@
 public class Gerente extends Funcionario{
+     //Declaração de classe Gerente, junta herença;
+     //Encapsulamento da variável login;
 	private String login;
+    //Encapsulamento da variável senha;
 	private String senha;
-	
+	//Consultar login pelo método get;
 	public String getLogin() {
 		return login;
 	}
+     //Atribuir login pelo método set
 	public void setLogin(String login) {
 		this.login = login;
 	}
+    //Consultar senha pelo método get;
 	public String getSenha() {
 		return senha;
 	}
+     //Atribuir senha pelo método set
 	public void setSenha(String senha) {
 		this.senha = senha;
 	}
-		
+	//Calculando o valor atual do obejto salário * 0.15;	
 	public double bonifica(){
 		return this.salario * 0.15;
 	}
 	
-	
+	//Autenticação de acesso, impressão de permitido ou negado;
 	public void autentica(String login, String senha) {
 		if((this.login == login) && (this.senha == senha)) {
 			System.out.println("Acesso Permitido!");
